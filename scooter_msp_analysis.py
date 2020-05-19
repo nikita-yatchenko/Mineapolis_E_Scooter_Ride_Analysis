64faa160d3804bab414127bb8910cb0934c0f3f5
nikita-yatchenko <60195049+nikita-yatchenko@users.noreply.github.com>
2020-05-18 19:06:59 -0500

    Mistake in the dataset
    Found a 5 hour discrepancy between what MSP database was showing online vs what was available for csv download. Fixed 5 hour difference

--- scooter_msp_analysis.py
+++ scooter_msp_analysis.py
@@ -11,24 +11,28 @@ import numpy as np
 ### Get scooter data for September and October from http://opendata.minneapolismn.gov/
 sept_data = pd.read_csv('https://opendata.arcgis.com/datasets/deb23b7c4983427aa83881db0e5ee6ad_0.csv')
 oct_data = pd.read_csv('https://opendata.arcgis.com/datasets/70977c11cc9b419fa5856351be721fbd_0.csv')
+nov_data = pd.read_csv('https://opendata.arcgis.com/datasets/01cc4e6abf5f40398efc299585dc9775_0.csv')
 
-scooter_data = pd.concat([sept_data, oct_data], join='outer')
-scooter_data = scooter_data[scooter_data.EndCenterlineID.map(len) < 9]
+scooter_data = pd.concat([sept_data, oct_data, nov_data], join='outer')
+scooter_data = scooter_data[scooter_data.EndCenterlineID.map(len) < 10]
 scooter_data['EndCenterlineID'] = scooter_data['EndCenterlineID'].astype('float64').astype('int64')
-
+scooter_data.sort_values(by='StartTime', inplace=True)
+scooter_data.StartTime = pd.to_datetime(scooter_data.StartTime) - pd.Timedelta(hours=5)
 ### Get GBSID (http://opendata.minneapolismn.gov/) street identification and Merge with Scooter
 gbsid = pd.read_csv('https://opendata.arcgis.com/datasets/43b425de3d4f444f96a5bf6605e3081b_3.csv', usecols=['GBSID','STREET_F_N', 'STREET_O_N'])
+gbsid.drop_duplicates(subset='GBSID', keep='first', inplace=True)
 gbsid.dropna(subset = ['STREET_F_N', 'STREET_O_N'], inplace = True)
 gbsid['intersection'] = gbsid.STREET_O_N.str.cat(gbsid.STREET_F_N, sep=' and ')
 
 scooter_street_data = pd.merge(scooter_data, gbsid, how = 'left', left_on = 'EndCenterlineID', right_on = 'GBSID')
 scooter_street_data.dropna(subset=['intersection'], inplace=True)
-
+scooter_street_data.StartTime= pd.to_datetime(scooter_street_data.StartTime)
+####
 ### Obtain avg. # of trips per hour for every day
 scoot_group_daily = scooter_street_data.groupby(by = 'StartTime')['TripID', 'intersection'].agg({'TripID' : 'count', 'intersection': lambda x: x.value_counts().index[0]})
 scoot_group_daily['StartTime'] = scoot_group_daily.index
 scoot_group_daily.rename(columns={'TripID':'NumberTrips'}, inplace=True)
-scoot_group_daily['StartTimeH'] = scoot_group_daily.StartTime.str.split(' ', expand = True)[1]
+scoot_group_daily['StartTimeH'] = scoot_group_daily.StartTime.astype('str').str.split(' ', expand = True)[1]
 
 ### Then obtain avg. # of trips per hour and the most frequent destination for the day
 scoot_group_hourly = scoot_group_daily.groupby(by = 'StartTimeH')['NumberTrips', 'intersection'].agg({'NumberTrips' : 'mean', 'intersection': lambda x: x.value_counts().index[0]})
@@ -44,7 +48,7 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.xlabel('Hours')
 plt.ylabel('Average Number of E-Scooter Rides')
-ax.set_title('Hourly Average Number of E-Scooter Rides for September and October in Minneapolis, MN, USA', fontsize = 14)
+ax.set_title('Hourly Average Number of E-Scooter Rides for September - November in Minneapolis, MN, USA', fontsize = 14)
 plt.text(0, 250, 'Most Common Destination at {} hours is {}'.format(scoot_group_hourly.index[0], scoot_group_hourly.intersection[0]))
 
 
